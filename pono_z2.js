f45876ed7cce64ecbf204a7e7d15e61ad62c30b7
ViktorPon <gogelikgo@gmail.com>
2026-09-10 10:04:04 +0200

    Add files via upload

---
+++ pono_z2.js
@@ -0,0 +1,5 @@
+document.write("Moj numer w dzienniku to 16"+"<br>");
+document.write("Wiersz pierwszy"+ "&nbsp;" + "&nbsp;" + "15" +"&nbsp;"+"&nbsp;"+"Nie"+"&nbsp&nbsp&nbsp"+"Tak"+"<br>");
+document.write("Wiersz drugi"+ "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" + "&nbsp;" + "15" +"&nbsp;"+"&nbsp;"+"Nie"+"&nbsp&nbsp&nbsp"+"Tak"+"<br>");
+document.write("ostatnia modyfikacja strony".fontcolor("#008080 ").strike().fontsize(7)+"<br>");
+document.write(document.lastModified.fontcolor("#008080 ").strike().fontsize(7)+"<br>");        
